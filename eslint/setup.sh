5e6a6d82bcf8fc23ed1c22ef3c0746fe206b9761
pqppq <yutatsu0119@gmail.com>
2022-11-09 08:54:13 +0900

    chore: add husky and lint-staged, commitlint

--- eslint/setup.sh
+++ eslint/setup.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+# setup eslint
 npm install --save-dev eslint typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-config-prettier eslint-plugin-unused-imports # eslint-plugin-jest
 npm install --save-dev --save-exact prettier
+# edit .eslintrc.json and .prettierrc
+
+# setup lint-staged
+npm install --save-dev lint-staged
+# edit .lintstagedrc
+
+# setup husky
+npm install --save-dev husky
+npm pkg set scripts.prepare="husky install"
+npm run prepare
+npx husky add .husky/pre-commit "npx lint-staged"
+
+# setup commitlint
+npm install --save-dev @commitlint/{cli,config-conventional}
+echo "module.exports = { extends: ['@commitlint/config-conventional'] };" > commitlint.config.js
+npx husky add .husky/commit-msg  'npx --no -- commitlint --edit ${1}'

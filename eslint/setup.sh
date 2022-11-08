9dcb43cb4c726a7672a81b4d2a0a5ebb5ff1a8d9
pqppq <yutatsu0119@gmail.com>
2022-11-09 08:41:29 +0900

    chore: add eslint setup script

---
+++ eslint/setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+npm install --save-dev eslint typescript @typescript-eslint/parser @typescript-eslint/eslint-plugin eslint-config-prettier eslint-plugin-unused-imports # eslint-plugin-jest
+npm install --save-dev --save-exact prettier
